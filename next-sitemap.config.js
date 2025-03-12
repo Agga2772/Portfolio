e744355e657a63b99243bb6575a295ca0cf6bc97
Agga Aung <aggaaung202@gmail.com>
2025-03-12 14:31:01 +0000

    Added next-sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://agga-portfolio.vercel.app", // Change this to your actual domain when you have one
+    generateRobotsTxt: true, // Generates a robots.txt file
+    sitemapSize: 5000,
+  };
+  
